1a9e9fda99d9f381ef23473d3ffe8a27194ff227
nikania <nianemis@gmail.com>
2024-01-27 16:41:57 -0300

    updated download

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,33 @@ import { Button, Stack } from "@chakra-ui/react";
 
 function App() {
   const handleDownload = () => {
-    fetch(
-      "http://localhost:8080/download?name=img-3ecd19d4-0240-4bac-b6b7-c56c34547018.jpeg",
-    )
-      .then((resp) => resp.json())
-      .then((data) => console.log(data))
+    const name: string = "img-3ecd19d4-0240-4bac-b6b7-c56c34547018.jpeg";
+    fetch(`http://localhost:8080/download?name=${name}`)
+      .then((resp) => {
+        return resp.blob();
+      })
+      .then((blob) => {
+        const link = document.createElement("a");
+        link.download = name;
+        link.href = URL.createObjectURL(blob);
+        link.click();
+
+        URL.revokeObjectURL(link.href);
+      })
       .catch((err) => console.log(err));
   };
-  const handleUpload = () => {};
+  const handleUpload = () => {
+    let blob = new Blob(["Hello, world!"], { type: "text/plain" });
+    fetch("http://localhost:8080/upload", {
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data;boundary=-",
+      },
+      body: blob,
+    })
+      .then((resp) => alert(resp.headers))
+      .catch((err) => console.log(err));
+  };
   function handleResize(
     event: MouseEvent<HTMLButtonElement, MouseEvent>,
   ): void {
@@ -65,9 +84,26 @@ function App() {
         >
           Download
         </Button>
-        <Button leftIcon={<ArrowUpIcon />} onClick={handleUpload}>
-          Upload
-        </Button>
+        <form
+          method="post"
+          // action="http://localhost:8080/upload"
+          encType="multipart/form-data"
+        >
+          <div>
+            <label htmlFor="file">Choose a file</label>
+            <input type="file" id="file" name="myFile" />
+          </div>
+          <div>
+            <Button
+              type="submit"
+              leftIcon={<ArrowUpIcon />}
+              onClick={handleUpload}
+            >
+              Upload
+            </Button>
+          </div>
+        </form>
+
         <Button onClick={handleResize}>Resize</Button>
         <Button onClick={handleConvert}>Convert</Button>
         <Button onClick={handleDelete}>Delete</Button>
