5d6bf9e902bc6b97c330f2e39ecec1202e866c48
nikania <nianemis@gmail.com>
2024-06-05 21:36:35 -0300

    implemented action chain structs

--- server/pkg/handler/auth_chain.go
+++ server/pkg/handler/auth_chain.go
@@ -1,19 +1,39 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
-type Action int
+type ActionType string
 
 const (
-	Resize Action = iota
-	Crop
-	Convert
-	Compress
+	Resize   ActionType = "resize"
+	Crop     ActionType = "crop"
+	Convert  ActionType = "convert"
+	Compress ActionType = "compress"
 )
+
+type Params struct {
+	Format              string `json:"format,omitempty"`
+	Width               int    `json:"width,omitempty"`
+	Height              int    `json:"height,omitempty"`
+	MaintainAspectRatio bool   `json:"maintainAspectRatio,omitempty"`
+	X                   int    `json:"x,omitempty"`
+	Y                   int    `json:"y,omitempty"`
+	Quality             int    `json:"quality,omitempty"`
+}
+
+type Action struct {
+	Name   ActionType `json:"name"`
+	Params Params     `json:"params"`
+}
+
+type ImageProcessingPipeline struct {
+	Actions []Action `json:"actions"`
+}
 
 func (h *Handler) chain(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w, r)
@@ -33,8 +53,34 @@ func (h *Handler) chain(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	// decode body
+	var pipeline ImageProcessingPipeline
+	err = json.NewDecoder(r.Body).Decode(&pipeline)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
 
 	// perform chain of actions
+
+	// Process the actions in the given order
+	for _, action := range pipeline.Actions {
+		switch action.Name {
+		case Convert:
+			h.services.Images.Convert(file, action.Params.Format)
+			fmt.Printf("Converting to %s format\n", action.Params.Format)
+		case Resize:
+			h.services.Images.Resize(file, action.Params.Width, action.Params.Height, action.Params.MaintainAspectRatio)
+			fmt.Printf("Resizing to %dx%d (maintain aspect ratio: %t)\n", action.Params.Width, action.Params.Height, action.Params.MaintainAspectRatio)
+		case Crop:
+			h.services.Images.Crop(file, action.Params.X, action.Params.Y, action.Params.Width, action.Params.Height)
+			fmt.Printf("Cropping from (%d, %d) with dimensions %dx%d\n", action.Params.X, action.Params.Y, action.Params.Width, action.Params.Height)
+		case Compress:
+			h.services.Images.Compress(file, action.Params.Quality)
+			fmt.Printf("Compressing with quality %d\n", action.Params.Quality)
+		default:
+			fmt.Printf("Unknown action: %v\n", action.Name)
+		}
+	}
 
 	// encode response
 
